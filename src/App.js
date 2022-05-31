b99b7b2f9e55781c6cc55041e66cdee4807abd86
Basmulla <basmulla1087@gmail.com>
2022-05-31 15:57:15 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { HashRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import {EditBookPage, HomePage} from './containers';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" render={() => (
-            <Navigate to="/book-directory-client" />
-          )} />
           <Route exact path="/book-directory-client" element={<HomePage/>} />
         <Route exact path="/book-directory-client/:id" element={<EditBookPage/>} />
       </Routes>
