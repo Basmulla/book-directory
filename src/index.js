17bd31aef53b7de0e1ad7db7964c8e339a771704
Basmulla <basmulla1087@gmail.com>
2022-06-10 13:30:03 -0400

    removed index.css from index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import './index.css';
 import App from './App';
 
 const store = configureStore();
