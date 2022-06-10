071a09fa011478b6e14529f687c6e051000ad296
Basmulla <basmulla1087@gmail.com>
2022-06-10 13:14:35 -0400

    adding images file for header and adding logo for tab

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React from 'react';
 import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
     render() {
         return (
             <header>
-                    <div className="topnav">
-                        <NavLink to="/book-directory-client" activeclassname="active">Home Page</NavLink>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+                <div class="topnav">
+                    <img src={process.env.PUBLIC_URL + "/images/Atekulla_Basmulla Avatar.png"} width="45" height="45" alt="Avatar" />
+                    &nbsp;
+                    <a><NavLink to="/book-directory-client" activeclassname="active"><i class="fa fa-fw fa-home"></i>Home</NavLink></a>
+                    &nbsp;
                     </div>
-                <h2>Book Directory API Website</h2>
+                <h2><center>Book Directory API Website</center></h2>
             </header>
         );
     }
